a283f3986d9d6dfa086c6218c96c53434408f373
Pillar <wangshangxiake@126.com>
2023-09-29 19:50:54 +0800

    feat: 员工新建功能中，解析token获取当前登录用户，使用ThreadLocal实现传参（变量共享）

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -60,8 +61,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
-    public void
-    save(EmployeeDTO employeeDTO){
+    public void save(EmployeeDTO employeeDTO){
         Employee employee = new Employee();
 
         // 对象属性拷贝
@@ -70,9 +70,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
-        // TODO: 后续改成当前登录用户的id
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
     }
